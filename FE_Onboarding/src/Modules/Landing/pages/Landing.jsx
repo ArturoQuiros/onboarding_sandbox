c1283d428e502f101ca0745d5c9d8d7233adb8cd
Arturo Quiros <56704525+ArturoQuiros@users.noreply.github.com>
2025-09-23 22:23:29 -0600

    Botones para Login

--- FE_Onboarding/src/Modules/Landing/pages/Landing.jsx
+++ FE_Onboarding/src/Modules/Landing/pages/Landing.jsx
@@ -4,6 +4,7 @@ import { Navbar } from "../components";
 import styles from "./Landing.module.css";
 import bgImage from "../../../Global/assets/onboarding_bg.jpg";
 import logo from "../../../Global/assets/onboarding_logo.png";
+import { Link } from "react-router-dom";
 
 export const Landing = () => {
   return (
@@ -14,7 +15,25 @@ export const Landing = () => {
         style={{ backgroundImage: `url(${bgImage})` }}
       >
         <div className={styles.overlay}></div>
-        <img src={logo} alt="Onboarding Logo" className={styles.logo} />
+        <div className={styles.content}>
+          <img src={logo} alt="Onboarding Logo" className={styles.logo} />
+          <div className={styles.buttonRow}>
+            {/* Contenedor del primer botón */}
+            <div className={styles.buttonContainer}>
+              <p className={styles.buttonText}>¿Eres parte del STAFF?</p>
+              <Link to="/staff-login" className={styles.button}>
+                Iniciar Sesión
+              </Link>
+            </div>
+            {/* Contenedor del segundo botón */}
+            <div className={styles.buttonContainer}>
+              <p className={styles.buttonText}>¿Eres Cliente?</p>
+              <Link to="/client-login" className={styles.button}>
+                Iniciar Sesión
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
