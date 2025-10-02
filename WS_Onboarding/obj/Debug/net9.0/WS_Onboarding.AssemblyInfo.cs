836fbbce0a6596f803ba6b344a304b39a009a824
Arturo Quiros <56704525+ArturoQuiros@users.noreply.github.com>
2025-10-01 21:07:48 -0600

    Restaurando Microsoft.EntityFrameworkCore
    reparando extensión rota

--- WS_Onboarding/obj/Debug/net9.0/WS_Onboarding.AssemblyInfo.cs
+++ WS_Onboarding/obj/Debug/net9.0/WS_Onboarding.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WS_Onboarding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f8c18224eb98209da0f82405f24919339a8f1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WS_Onboarding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WS_Onboarding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
