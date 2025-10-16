77ab518da8d4824b6930e978ae1093f7490357b7
Arturo Quiros <56704525+ArturoQuiros@users.noreply.github.com>
2025-10-15 22:19:14 -0600

    ajustes en navbar de user

--- FE_Onboarding/src/Modules/Onboarding/components/Navbar.jsx
+++ FE_Onboarding/src/Modules/Onboarding/components/Navbar.jsx
@@ -1,16 +1,43 @@
-// src/Global/components/Navbar/Navbar.jsx
-import React from "react";
+import React, { useContext } from "react";
+import { BsTranslate } from "react-icons/bs";
+import { FiLogOut } from "react-icons/fi";
+import { LanguageContext } from "../../../Global/Context";
 import styles from "./Navbar.module.css";
+import { useTranslation } from "react-i18next";
+import { useAuth } from "../../../Global/hooks";
 
 export const Navbar = () => {
+  const { t, i18n } = useTranslation("global");
+  const { setLanguage } = useContext(LanguageContext);
+  const { logout } = useAuth(); // ✅ Solo se llama una vez
+
+  const handleLanguageToggle = () => {
+    setLanguage(i18n.language === "es" ? "en" : "es");
+  };
+
+  const handleLogOut = () => {
+    logout(); // Llama al logout del AuthContext
+  };
+
   return (
-    <header className={styles.header}>
+    <nav className={styles.navbar}>
       <img
         src="https://cdn.bdo.global/images/bdo_logo/1.0.0/bdo_logo_color.png"
         alt="BDO logo"
         className={styles.logo}
       />
       <h1 className={styles.title}>Costa Rica</h1>
-    </header>
+      <div className={styles.menu}>
+        <button className={styles.langButton} onClick={handleLanguageToggle}>
+          <BsTranslate className={styles.translateIcon} />
+          <span>{i18n.language === "es" ? "ES" : "EN"}</span>
+        </button>
+
+        <button className={styles.logoutButton} onClick={handleLogOut}>
+          <FiLogOut className={styles.logoutIcon} />
+          <span>{t("common.logout")}</span>
+        </button>
+      </div>
+    </nav>
   );
 };
